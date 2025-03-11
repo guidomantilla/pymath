3075f36cda8ff57662ba10b0a0e0a4f610b0598d
Guido Mantilla Tarazona <raven@macbook-pro-m1-max.local>
2025-03-10 23:22:43 -0500

    avance

--- pkg/logica/functions.py
+++ pkg/logica/functions.py
@@ -4,14 +4,17 @@ from IPython.display import display, Latex
 from sympy.logic.boolalg import truth_table
 
 
-def print_latex(expr):
+def print_latex(expr, key: str = None):
     expression = "$$\\Huge {}$$".format(latex(expr))
+    if key is not None:
+        expression = "$$\\Huge {}: {}$$".format(key, latex(expr))
     display(Latex(expression))
 
 
 def print_truth_table(expr, symbols):
     atoms = ",".join(map(str, symbols))
     table = truth_table(expr, symbols)
+
     for t in table:
 
         inputs = ""
---
+++ pkg/relaciones/functions.py
@@ -0,0 +1,39 @@
+import sympy as sp
+from sympy.core.relational import Relational
+
+from pkg.logica.functions import print_latex
+
+
+def transitive(p: Relational, q: Relational, latex=False, debug=False) -> Relational:
+
+    if p.__class__ != q.__class__:
+        raise TypeError("Both relational expression must be of the same type")
+
+    relational_operator = p.__class__
+
+    if not p.rhs.equals(q.lhs):
+        raise TypeError("Both relational expression must be ready for transitive operation")
+
+
+    ab_sum = relational_operator(p.lhs - p.rhs, p.rhs - p.rhs)
+    bc_sum = relational_operator(q.lhs - q.rhs, q.rhs - q.rhs)
+
+    lhs_sum = sp.Add(ab_sum.lhs, bc_sum.lhs, evaluate=False)
+    rhs_sum = sp.Add(ab_sum.rhs, bc_sum.rhs, evaluate=False)
+    sum = relational_operator(lhs_sum, rhs_sum)
+
+    if latex:
+        print_latex(ab_sum)
+        print_latex(bc_sum)
+        print_latex(sum)
+        print_latex(sum.evalf())
+        print_latex(sum.simplify())
+
+    if debug:
+        print(f"ab_sum: {ab_sum}")
+        print(f"bc_sum: {bc_sum}")
+        print(f"sum: {sum}")
+        print(f"sum: {sum.evalf()}")
+        print(f"sum: {sum.simplify()}")
+
+    return sum.simplify()
